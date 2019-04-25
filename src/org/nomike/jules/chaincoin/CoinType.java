7ef58795810f2960f9ea930e1655145ee4d2c8ce
jules <julia.djanic@stud.fh-campuswien.ac.at>
2019-04-25 02:45:14 +0200

    added license, updated input loop, updated input dialogue

--- src/org/nomike/jules/chaincoin/CoinType.java
+++ src/org/nomike/jules/chaincoin/CoinType.java
@@ -1,3 +1,23 @@
+/* A Coin Collector, adding up values of coins, demonstrating use of Chain of Responsibility
+    Copyright (C) 2019  Amal Alaswad <amal.alaswad@stud.fh-campuswien.ac.at>
+    					Julia Djanic <julia.djanic@stud.fh-campuswien.ac.at>
+    					Melanie Grotti <melanie.grotti@stud.fh-campuswien.ac.at>
+    					Elisabeth Kohlhofer <elisabeth.kohlhofer@stud.fh-campuswien.ac.at>
+    					Sandra Stojkov <sandra.stojkov@stud.fh-campuswien.ac.at>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.nomike.jules.chaincoin;
 
 public class CoinType extends CoinHandler {
--- src/org/nomike/jules/chaincoin/Main.java
+++ src/org/nomike/jules/chaincoin/Main.java
@@ -1,31 +1,60 @@
+/* A Coin Collector, adding up values of coins, demonstrating use of Chain of Responsibility
+    Copyright (C) 2019  Amal Alaswad <amal.alaswad@stud.fh-campuswien.ac.at>
+    					Julia Djanic <julia.djanic@stud.fh-campuswien.ac.at>
+    					Melanie Grotti <melanie.grotti@stud.fh-campuswien.ac.at>
+    					Elisabeth Kohlhofer <elisabeth.kohlhofer@stud.fh-campuswien.ac.at>
+    					Sandra Stojkov <sandra.stojkov@stud.fh-campuswien.ac.at>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package org.nomike.jules.chaincoin;
 
 import java.util.Scanner;
 
 public class Main {
+	static CoinChain coinChain = new CoinChain();
+	static Scanner scanner = new Scanner(System.in);
     
     public static void main(String args[]) {
-    	CoinChain coinChain = new CoinChain();
-    	Scanner scanner = new Scanner(System.in);
-    	
+
     	System.out.println("==Welcome to your Coin Collector==\n"
-    			+ " ==Please insert your Coin here==\n"
+    			+ " ==Please insert your Coin here==\n\n"
     			+ "                ++\n"
     			+ "                ||\n"
     			+ "                ||\n"
-    			+ "                ++\n");
+    			+ "                ++\n\n"
+    			+ "     ==(type \"esc\" to exit)==");
  
     	while (true) {
     		if(scanner.hasNextDouble()) {
     			double coin = scanner.nextDouble();
     			coinChain.addCoin(coin);
-    	    	coinChain.printTotal();
     		} else {
-    			System.out.println("Not a Coin. Please try it with actual Currency (double value)");
-    			break;
+    			String input = scanner.next();
+    			if ("esc".equals(input)) {
+    				break;
+    			} else { 
+    				System.out.println("...Not a Coin.\n"
+    						+ "Please try it with actual Currency (double value).\n"
+    						+ "To exit, type \"esc\" (without the quotation marks :P)\n");
+    			}
     		}
     	}
-    	scanner.close();
-//    	coinChain.printTotal();
-    }
+    	
+        scanner.close();
+    	System.out.println("Thanks for Piggy Banking!\n"
+    			+ "This is what you collected today:\n");
+    	coinChain.printTotal();
+    	}
 }
